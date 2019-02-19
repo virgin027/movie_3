7873ecac300f39011904d2973e1f09e948eac0ee
JCoderius <jf.hoarau@laposte.net>
2019-02-19 12:43:13 +0100

    modification fichier index.php

--- index.php
+++ index.php
@@ -3,10 +3,29 @@ include('inc/fonctions.php');
 include('inc/request.php');
 include('inc/pdo.php');
 
+$success = false;
+$errors = array();
+
+$sql = "SELECT *
+        FROM movies_full";
+$query = $pdo->prepare($sql);
+$query->execute();
+$movies = $query->fetchAll();
+// debug($movies);
 
 
+// debug($errors);
 
 include('inc/header.php');?>
+<section id="film">
+  <div class="wrap">
+    <?php  foreach ($movies as $movie) {?>
+      <div class="images">
+        <a href="detail.php?=id <?php echo $movie['id'] ?>"><img src="posters/<?php echo $movie['id'] ?>.jpg" alt="<?php echo $movie['id'] ?>"></a>
+      </div>
+   <?php } ?>
+  </div>
+</section>
 
 
 
